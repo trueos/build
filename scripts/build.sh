fb2ac20a9c1077fd5d52e7280000cf8d44ddfec0
Kris Moore <kris@ixsystems.com>
2019-04-17 09:53:19 -0400

    Fix updating git / poudriere ports if there are local changes, also update pkgbase image to use bsdinstall

--- scripts/build.sh
+++ scripts/build.sh
@@ -343,9 +343,12 @@ is_ports_dirty()
 		return 1
 	fi
 
+	# Need to make sure ports is clean before updating, poudriere won't return non-0
+	# if it fails
+	(cd ${PDIR} && git reset --hard)
+
 	# Looks like we are safe to try an in-place upgrade
 	echo "Updating previous poudriere ports tree"
-	poudriere ports -u -p ${POUDRIERE_PORTS}
 	if [ $? -ne 0 ] ; then
 		echo "Failed updating, checking out ports fresh"
 		echo -e "y\n" | poudriere ports -d -p ${POUDRIERE_PORTS}
